80b15a3be480bb7aff5d9162c79d33e648849865
unknown <srivisionguide@gmail.com>
2021-05-25 22:54:07 +0530

    setting up envirnoment for rasa chat window with update

--- RasaChatWin/Form1.cs
+++ RasaChatWin/Form1.cs
@@ -47,8 +47,8 @@ namespace RasaChatWin
                 Label1.BackColor = System.Drawing.Color.White;
                 Label1.Text = message;
                 Label1.AutoSize = true;
-                Label1.MinimumSize = new System.Drawing.Size(x-15, 0);
-                Label1.Size = new System.Drawing.Size((x-15), 10);
+                Label1.MinimumSize = new System.Drawing.Size(x-25, 0);
+                Label1.Size = new System.Drawing.Size((x-25), 10);
                 if (responsetype == "user")
                 {
                     Label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -72,8 +72,8 @@ namespace RasaChatWin
                 Label2.Text = message;
                 Label2.ForeColor = System.Drawing.Color.Navy;
                 Label2.AutoSize = true;
-                Label2.MinimumSize = new System.Drawing.Size(x - 15, 0);
-                Label2.Size = new System.Drawing.Size((x - 15), 10);
+                Label2.MinimumSize = new System.Drawing.Size(x - 25, 0);
+                Label2.Size = new System.Drawing.Size((x - 25), 10);
                 if (responsetype == "user")
                 {
                     Label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -86,12 +86,7 @@ namespace RasaChatWin
                 Label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 
                 flowLayoutPanel1.Controls.Add(Label2);
-
             }
-
-
-
-
         }
 
         private void userbutton_Click(object sender, EventArgs e)
@@ -100,7 +95,7 @@ namespace RasaChatWin
             var query = userbox.Text;
             Console.WriteLine(query);
             dynamicLabels("link", "user", query);
-            dynamicLabels("Link", "server", query);
+            dynamicLabels("link", "server", query);
             dynamicLabels("regular", "user", query);
             dynamicLabels("regular", "server", query);
 
